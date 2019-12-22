e54fa07489c4051d350030325a2727fa4686aaf1
virak0001 <virakcambodia44@gmail.com>
2019-12-22 12:50:06 +0700

    homework

---
+++ Controllers/student_controller.php
@@ -0,0 +1,111 @@
+<?php
+$data = array();
+get_action($data);
+
+function get_action(&$data){
+    $function = 'view';
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+        $function = $action;
+    }
+    $function($data);
+}
+
+function edit(&$data) {
+    $data['student'] = m_edit($_GET);
+    $data['subject_data'] = get_subject_edit_data();
+    $data['page'] = "students/edit";
+}
+
+function view(&$data) {
+    $data['student_data'] = m_get_data();
+    // var_dump($data['student_data']);die();
+    $data['page'] = "students/view";
+}
+
+function add(&$data) {
+    $data['class_data'] = get_class_data();
+    $data['subject_data'] = get_subject_data();
+    $data['page'] = "students/add";
+}
+
+function form_data(&$data) {
+    $add_data = student_add_data($_POST);
+    if($add_data) {
+        $action = "view";
+    }else {
+        $action = "add";
+    }
+    header("Location: index.php?action=$action");
+}
+
+function delete(&$data) {
+    $delete = delete_data($_GET);
+    if($delete) {
+        $action = "view";
+    }
+    header("Location: index.php?action=$action");
+}
+
+function edit_data(&$data) {
+    $update_data = m_update_data($_POST);
+    if($update_data) {
+        $action = "view";
+    }else {
+        $action = "edit";
+    }
+    header("Location: index.php?action=$action");
+}
+
+
+
+
+
+
+
+
+// function add(&$data) {
+//     $data['page'] = "students/add";
+// }
+
+
+// function detail(&$data) {
+//     //code here
+//     $data['student'] = m_detail();
+//     $data['page'] = "students/detail";
+// }
+
+// function delete(&$data) {
+//     //code here
+//     $result = m_delete();
+//     if($result) {
+//         $action = "view";
+//     }else {
+//         echo "Cannot delete this record!!!";
+//     4}
+//     header("Location: index.php?action=$action");
+// }
+
+// function edit(&$data) {
+//     //code here
+//     $data['student'] = m_detail();
+//     $data['page'] = "students/edit";
+// }
+
+
+
+// function update_profile(&$data) {
+//     $data['student'] = m_detail();
+//     $data['page'] = "students/changeProfile";
+// }
+
+// function change_profile(&$data) {
+//     $change_profile = m_change_profile($_POST);
+//     $id = $_GET['id'];
+//     if($change_profile) {
+//         $action = "detail&id=$id";
+//     }else {
+//         $action = "edit";
+//     }
+//     header("Location: index.php?action=$action");
+// }
---
+++ Views/students/edit.php
@@ -0,0 +1,63 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header bg-primary text-center text-white">
+                    <h2>Student Information Form</h2>
+                </div>
+                <?php
+                if (isset($data['student'])) {
+                    foreach ($data['student'] as $row) {
+                ?>
+                        <form action="index.php?action=edit_data&" method="post" enctype="multipart/form-data">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="name">Firstname</label>
+                                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                    <input type="text" name="firstname" id="name" value="<?php echo $row['firstname'] ?>" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">Lastname</label>
+                                    <input type="text" name="lastname" id="name" value="<?php echo $row['lastname'] ?>" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="class">Class:</label>
+                                    <select name="class" id="class" class="form-control">
+                                        <option <?php if ($row['title'] == 'WEP2020-A') { ?> selected="selected" <?php } ?> value="WEP2020-A">WEP2020-A</option>
+                                        <option <?php if ($row['title'] == 'WEP2020-B') { ?> selected="selected" <?php } ?> value="WEP2020-B">WEP2020-B</option>
+                                        <option <?php if ($row['title'] == 'SNA2020') { ?> selected="selected" <?php } ?> value="SNA2020">SNA2020</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="sex">Gender:</label><br>
+                                    <input type="radio" <?php if ($row['sex'] == 'Male') { ?> checked="checked" <?php } ?> name="sex" value="Male">Male <br>
+                                    <input type="radio" <?php if ($row['sex'] == 'Female') { ?> checked="checked" <?php } ?> name="sex" value="Female">Female
+                                </div>
+                                <div class="form-group">
+                                    <label for="subject">Subjects:</label>
+                                    <select name="subjects[]" class="form-control" multiple>
+                                        <?php
+                                        foreach ($data['subject_data'] as $rows) {
+                                        ?>
+                                            <option value="<?php echo $rows['id']; ?>"><?php echo $rows['sub_title']; ?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <a href="index.php?action=view" class="btn btn-success">Go Back</a>
+                                <input type="submit" name="create" value="Submit" class="btn btn-primary float-right">
+                            </div>
+                        </form>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
